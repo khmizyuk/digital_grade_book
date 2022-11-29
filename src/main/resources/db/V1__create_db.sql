15663820ad971a721f46f887a2cabaa5ff7a286e
khmizyuk <hmizyk2002@gmail.com>
2022-11-29 17:25:57 +0300

    db fix

--- src/main/resources/db/V1__create_db.sql
+++ src/main/resources/db/V1__create_db.sql
@@ -1,21 +1,20 @@
-CREATE TABLE user (
-    id          SERIAL          NOT NULL,
-    email       VARCHAR(255)    NOT NULL,
-    password    VARCHAR(255)    NOT NULL,
-    firstName   VARCHAR(255)    NOT NULL,
-    lastName    VARCHAR(255)    NOT NULL,
-    accountType VARCHAR(255)    NOT NULL,
-    PRIMARY KEY (id)
-}
+CREATE TABLE user
+(
+    id          SERIAL       NOT NULL,
+    email       VARCHAR(255) NOT NULL,
+    password    VARCHAR(255) NOT NULL,
+    firstName   VARCHAR(255) NOT NULL,
+    lastName    VARCHAR(255) NOT NULL,
+    accountType VARCHAR(255) NOT NULL,
+    CONSTRAINT user_pk PRIMARY KEY (id)
+);
 
 CREATE TABLE grade_book (
     id          SERIAL          NOT NULL,
     date        DATETIME        NOT NULL,
-    mark        VARCHAR(255)   NOT NULL,
-    student_id  SERIAL          NOT NULL,
-    subject     VARCHAR(255)   NOT NULL,
-    teacher_id  SERIAL          NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (student_id) REFERENCES user(id),
-    FOREIGN KEY (teacher_id) REFERENCES user(id)
-)
+    mark        VARCHAR(255)    NOT NULL,
+    student_id  BIGINT          NOT NULL    REFERENCES user(id) ON UPDATE CASCADE ON DELETE CASCADE,
+    subject     VARCHAR(255)    NOT NULL    REFERENCES user(id) ON UPDATE CASCADE ON DELETE CASCADE,
+    teacher_id  BIGINT UNSIGNED          NOT NULL,
+    CONSTRAINT grade_book_pk PRIMARY KEY (id)
+);
