aa6344e1edeec9311ecef80ed8364b4dce10eb34
khmizyuk <hmizyk2002@gmail.com>
2022-11-29 16:44:52 +0300

    fix SERIAL

--- src/main/resources/db/V1__create_db.sql
+++ src/main/resources/db/V1__create_db.sql
@@ -1,5 +1,5 @@
 CREATE TABLE user (
-    id          SERIAL          NOT NULL AUTO_INCREMENT,
+    id          SERIAL          NOT NULL,
     email       VARCHAR(255)    NOT NULL,
     password    VARCHAR(255)    NOT NULL,
     firstName   VARCHAR(255)    NOT NULL,
@@ -9,7 +9,7 @@ CREATE TABLE user (
 }
 
 CREATE TABLE grade_book (
-    id          SERIAL          NOT NULL AUTO_INCREMENT,
+    id          SERIAL          NOT NULL,
     date        DATETIME        NOT NULL,
     mark        VARCHAR(255)   NOT NULL,
     student_id  SERIAL          NOT NULL,
